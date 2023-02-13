5fe7d1a9a101a4e73cce8781976aa98243693035
robinson truque <robinsontruque04@gmail.com>
2023-02-13 09:32:29 -0500

    juego de robinson

---
+++ data/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+      question: '¿cual de los siguien es una saiyan ?',
+      choices: ['naruto','goku','minato','sasuke'],
+      answer: 'goku',
+    },
+    {
+      question:'¿cual es organo del cuerpo humano que consume mas energia  ?',
+      choices:['dedos','corazón','cerebro','pies'],
+      answer: 'cerebro',
+  },
+  {
+    question:'¿ cual es la formula quimica del agua  ?',
+    choices: ['C6H12O6','C3H6','CH2O','H2O' ],
+    answer: 'H2O'},
+    {
+    question:'¿cual es el rio mas largo del mundo ?',
+    choices: ['Orinoco','Nilo','Cauca','Amazonas' ],
+    answer: 'Nilo'},
+    
+      
+  ];
+
+
+// export const data=[
+
+// {
+//     pregunta:'¿cual de los siguientes prota es de no es de naruto?',
+//     respuesta:['naruto','goku','minato','sasuke'],
+//     resultado: 'naruto'
+// },
+// {
+//     pregunta:'¿Que lenguaje  es para crear paginas web ?',
+//     respuesta:['nodejs','vue','word','postgres'],
+//     resultado: 'vue'
+// },
+// {
+//     preguntas:'¿Que lenguaje  es para backen ?',
+//     respuesta:['nodejs','boostrap','xampp','postgres' ],
+//     resultado: 'nodejs'
+// },
+
+// ];
